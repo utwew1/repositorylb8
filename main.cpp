5c9367ffd2ce4f7e0da9d7ab6fcddb18bf8f118d
utwew <bereshpolow99@mail.ru>
2021-06-06 23:35:40 +0300

    second commit

--- main.cpp
+++ main.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
 using namespace std;
+void input(int*, int*);
+
 int main()
 {
+    setlocale(LC_ALL, "rus");
+    int num1, nim2;
+
+    input(&num1, &num2);
+    cout <<"числа"<< num1 << "; " <<num2 << end1;
+
     return 0;
+}
+void input(int *a, int *b)
+{
+    cout <<"¬ведите первое число:";
+    cin >> *a;
+    cout <<"¬ведите второе число:";
+    cin >> *b;
 }
